52048c7a5bf7049a18ac14a5f20cbd3274a0c597
Marcin Kałużny <marcin.kaluzny@me.com>
2017-10-02 15:56:36 +0200

    przygotowanie

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,16 @@ const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
 
 const PATTERN = /\{\{(.*)\}\}/gm;
 
-function tpl(html, model) {
+function htmlModelSet(html, model) {
     return html.replace(PATTERN, ($0, $1) => model[$1] || $0);
 }
 
 function prepareElementsList(document) {
     const wrappers = document.body.children;
-    const elements = document.querySelectorAll("ref");
+    const elements = document.querySelectorAll("[ref]");
 
     return [...elements].reduce((obj, element) => {
-        const name = element.dataset.element;
+        const name = element.getAttribute("ref");
         obj[name] = element;
         return obj;
     }, {
@@ -23,5 +23,4 @@ function DOMParseFromString(str) {
     return (new DOMParser()).parseFromString(str, "text/html");
 }
 
-
-const TPL = compose(prepareElementsList, DOMParseFromString, tpl);
+const template = compose(prepareElementsList, DOMParseFromString, htmlModelSet);
