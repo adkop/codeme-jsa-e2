01f91600256823c4688d75894ea4273a0fa17f2f
Marcin Kałużny <marcin.kaluzny@me.com>
2017-10-10 17:29:57 +0200

    praca domowa task 1

---
+++ exercises/[11102017]/task1.js
@@ -0,0 +1,8 @@
+/**
+ * Zadanie 1.
+ * Proszę uzupelnij funkcje fetchData w komponencie listComponent tak,
+ * zeby jezeli nie ma w localStorage danych pobrala dane przez fetch
+ * i wstawila je do localSorage,
+ * jak dane sa w localStorage to dane pobierz z localStorage i wykonaj fetch sprawdz czy cos sie zmienilo
+ * jezeli tak uzupelnij dane i wyswietl je na stronie
+ */
