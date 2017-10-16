21b39224a10403dfacfed26abc948df19aceacdf
kaluznymarcin <32313395+kaluznymarcin@users.noreply.github.com>
2017-10-16 15:35:10 +0200

    Update sortable.js

--- src/js/sortable.js
+++ src/js/sortable.js
@@ -1,75 +1,92 @@
+import {css, on, off} from "./utils/dom";
+
 function sortable(listElement) {
-    function on(element, types, callback) {
-        types.split(/\s/).map(
-            type =>
-            element.addEventListener(type, callback, false)
-        );
-    }
-
-    function off(element, types, callback) {
-        types.split(/\s/).map(
-            type =>
-            element.removeEventListener(type, callback, false)
-        );
-    }
-
     // [...listElement.children].map();
     // "dragover dragenter dragleave"
+
     on(listElement, "mousedown", function (e) {
         const boxElement = e.target.closest("[role=listitem]");
- 
+
         if (!boxElement) {
             return false;
         }
-        
+
         boxElement.setAttribute("draggable", true);
 
         on(boxElement, "dragstart", onDragStart);
         on(boxElement, "drag", onDrag);
-
-        on(boxElement, "dragend drop", function (e) {
-            this.setAttribute("aria-grabbed", "false");
-            this.classList.remove("grabbed");
-            this.style.opacity = 0.3;
-            this.style.visibility = "hidden";
-        });
+        on(boxElement, "dragend", onDragEnd);
     });
+
+    on(listElement, "dragover dragenter", function (e) { e.preventDefault(); });
+    on(listElement, "drop", onDrop);
 
     on(listElement, "mouseup", function (e) {
         const boxElement = e.target.closest("[role=listitem]");
 
+        off(boxElement, "drag", onDrag);
         off(boxElement, "dragstart", onDragStart);
 
-        off(boxElement, "drag", onDrag);
-        
         boxElement.removeAttribute("draggable");
     });
 
-    function onDragStart(e) {
-        e.dataTransfer.setDragImage(this, 0, 0);
-        e.dataTransfer.dropEffect = "move";
-        e.dataTransfer.setData("text/plain", "lalala");
+    const listItems = [...listElement.querySelectorAll("[role=listitem]")].map(item => {
+        on(item, "dragover", onDragOver);
+        on(item, "dragenter", onDragEnter);
+        on(item, "dragleave", onDragLeave);
+        return item;
+    });
+
+    function onDragStart({dataTransfer}) {
+        // e.dataTransfer.setDragImage(this, 0, 0);
+        dataTransfer.effectAllowed = "move";
+        dataTransfer.dropEffect = "move";
+        setData(dataTransfer, this);
+
         this.setAttribute("aria-grabbed", "true");
         this.classList.add("grabbed");
     }
 
     function onDrag(e) {
-        const el = document.elementFromPoint(
-            e.pageX, e.pageY
-        );
+        e.dataTransfer.effectAllowed = "move";
+        e.dataTransfer.dropEffect = "move";
+        const el = document.elementFromPoint(e.pageX, e.pageY);
         const dragOverBoxElement = el.closest("[role=listitem]");
-        //console.log(dragOverBoxElement);
     }
 
-    function css(element, ...rest) {
-        let obj = rest[0];
-        if (1 < rest.length && "string" === typeof obj) {
-            obj = {
-                [rest[0]]: rest[1]
-            }
-        }
+    function onDragEnd(e) {
+        e.preventDefault();
+        e.dataTransfer.clearData();
 
-        Object.assign(element.style, obj);
+        this.setAttribute("aria-grabbed", "false");
+        this.classList.remove("grabbed");
+    }
+
+    function onDragEnter(e) {
+        e.stopPropagation();
+    }
+    function onDragLeave(e) {
+        e.stopPropagation();
+    }
+    function onDragOver(e) {
+        e.preventDefault();
+    }
+
+    function onDrop(e) {
+        e.preventDefault();
+        console.log("-----", e.type, "-----");
+        console.log(e.dataTransfer.getData("text/plain"));
+        console.log(e.dataTransfer.files);
+    }
+}
+
+function setData(dataTransfer, dragElement) {
+    var data = dragElement.getAttribute('aria-label') || dragElement.id || 'grabbed';
+    try {
+        dataTransfer.setData('text/plain', data);
+    } catch (ex) {
+        dataTransfer.clearData('Text');
+        dataTransfer.setData('Text', data);
     }
 }
 
